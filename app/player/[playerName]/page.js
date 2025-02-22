f82ef30a6c2aa6a123bc17b58f649733c8a53e71
Moburu <stevenschiffner3@gmail.com>
2025-02-22 16:28:44 -0500

    search function works

--- app/player/[playerName]/page.js
+++ app/player/[playerName]/page.js
@@ -1,26 +1,48 @@
 'use client'
 
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from "next/navigation"
 import styles from "../../page.module.css"
 
-const getPlayerSets = (name) => {
-    axios.get(`http://localhost:4000/player/${name}`).then((data) => {
-      console.log(data.data)
-      return data.data
-    })
-  }
+// const getPlayerSets = async (name) => {
+//     axios.get(`http://localhost:4000/player/${name}`).then((data) => {
+//         console.log(data.data)
+//         return data.data
+//     })
+// }
 
 export default function Player() {
     const { playerName } = useParams();
+    const [sets, setSets] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const sets = getPlayerSets(playerName)
+    useEffect(() => {
+        console.log(playerName)
+        const getPlayerSets = async (name) => {
+            try {
+            const response = await axios.get(`http://localhost:4000/player/${name}`).then((data) => {
+                return data
+            }
+            );
+                setSets(response.data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getPlayerSets(playerName)}, [])
 
     return (
         <div>
             <h1 className={styles.header}>{playerName}</h1>
             <br />
-            <p>{sets}</p>
+            {loading ? <p>Loading...</p> :
+            <ul>
+                {sets.map((set) => <li key={set.id}>{set.p1_tag} {set.p1_score} - {set.p2_score} {set.p2_tag}</li>)}
+            </ul>}
         </div>
     )
 }
