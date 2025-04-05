7ecd33ca63ccf2113f6fceeb31ff778b2b112527
Moburu <stevenschiffner3@gmail.com>
2025-04-05 16:52:32 -0400

    added graph selectors and styles

--- app/components/LineGraph.js
+++ app/components/LineGraph.js
@@ -18,21 +18,36 @@ export default function LineGraph(props) {
         const [data, setData] = useState({
             datasets: []
         });
+        const [placementData, setPlacementData] = useState({});
+        const [sprData, setSprData] = useState({});
         const [avgPlacementData, setAvgPlacementData] = useState({});
         const [avgSprData, setAvgSprData] = useState({});
         const [loading, setLoading] = useState(true);
-
-        let options = {
+        const [options, setOptions] = useState({
             responsive: true,
-            yAxes: [
-                {
-                    reverse: true
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'white'
+                    }
+                },
+                y: {
+                    reverse: true,
+                    ticks: {
+                        color: 'white'
+                    }
                 }
-            ],
+            },
             plugins: {
                 title: {
                     display: true,
-                    text: "Average Placement by Season"
+                    color: 'white',
+                    text: "Tournament Placements"
+                },
+                legend: {
+                    labels: {
+                        color: 'white'
+                    }
                 }
             },
             elements: {
@@ -41,7 +56,91 @@ export default function LineGraph(props) {
                     backgroundColor: "rgb(110, 41, 230)"
                 }
             }
-        };
+        })
+
+        const placementMode = () => {
+            setData(placementData);
+            setOptions({
+                ...options,
+                scales: {
+                    ...options.scales,
+                    y: {
+                        ...options.scales.y,
+                        reverse: true
+                    }
+                },
+                plugins: {
+                    ...options.plugins,
+                    title: {
+                        ...options.plugins.title,
+                        text: "Tournament Placement"
+                    }
+                }
+            });
+        }
+
+        const sprMode = () => {
+            setData(sprData);
+            setOptions({
+                ...options,
+                scales: {
+                    ...options.scales,
+                    y: {
+                        ...options.scales.y,
+                        reverse: false
+                    }
+                },
+                plugins: {
+                    ...options.plugins,
+                    title: {
+                        ...options.plugins.title,
+                        text: "SPR (Seed Performance Rating)"
+                    }
+                }
+            })
+        }
+
+        const avgPlacementMode = () => {
+            setData(avgPlacementData);
+            setOptions({
+                ...options,
+                scales: {
+                    ...options.scales,
+                    y: {
+                        ...options.scales.y,
+                        reverse: true
+                    }
+                },
+                plugins: {
+                    ...options.plugins,
+                    title: {
+                        ...options.plugins.title,
+                        text: "Average Placement by Season"
+                    }
+                }
+            })
+        }
+
+        const avgSprMode = () => {
+            setData(avgSprData);
+            setOptions({
+                ...options,
+                scales: {
+                    ...options.scales,
+                    y: {
+                        ...options.scales.y,
+                        reverse: false
+                    }
+                },
+                plugins: {
+                    ...options.plugins,
+                    title: {
+                        ...options.plugins.title,
+                        text: "Average SPR by Season"
+                    }
+                }
+            })
+        }
 
         ChartJS.register(
             CategoryScale,
@@ -74,7 +173,7 @@ export default function LineGraph(props) {
                 const placingBySeason = dataHolder.map(season => season.avgPlacement)
                 const sprBySeason = dataHolder.map(season => season.avgSpr);
 
-                const placementData = {
+                const avgPlacementObject = {
                     labels: uniqueSeasons,
                     datasets: [
                         {
@@ -85,7 +184,7 @@ export default function LineGraph(props) {
                     ]
                 }
 
-                const sprData = {
+                const avgSprObject = {
                     labels: uniqueSeasons,
                     datasets: [
                         {
@@ -96,9 +195,48 @@ export default function LineGraph(props) {
                     ]
                 }
 
-                setAvgPlacementData(placementData);
-                setAvgSprData(sprData);
-                setData(placementData);
+                setAvgPlacementData(avgPlacementObject);
+                setAvgSprData(avgSprObject);
+
+                const sortedTournaments = tournaments.sort((a, b) => {
+                    if (a.season === b.season) {
+                        return a.episode - b.episode
+                    } else {
+                        return a.season - b.season
+                    }
+                })
+
+                const seasonsArray = sortedTournaments.map(tournament => `S${tournament.season}E${tournament.epsiode}`)
+                const placements = sortedTournaments.map(tournament => tournament.placement);
+                const sprs = sortedTournaments.map(tournament => tournament.spr);
+
+                const placementObject = {
+                    labels: seasonsArray,
+                    datasets: [
+                        {
+                            label: "Placement",
+                            data: placements,
+                            borderColor: "rgb(110, 41, 230)"
+                        }
+                    ]
+                }
+
+                const sprObject = {
+                    labels: seasonsArray,
+                    datasets: [
+                        {
+                            label: "SPR",
+                            data: sprs,
+                            borderColor: "rgb(110, 41, 230)"
+                        }
+                    ]
+                }
+
+
+                setPlacementData(placementObject);
+                setSprData(sprObject);
+
+                setData(placementObject);
             }
 
             setLoading(true);
@@ -111,7 +249,15 @@ export default function LineGraph(props) {
             { loading ?
             <LoadingSpinner />
             :
-            <Line options={options} data={data} />
+            <div>
+                <div className="flex flex-row content-center justify-center gap-2 mb-5">
+                    <button onClick={placementMode}>Placement</button>
+                    <button onClick={sprMode}>SPR</button>
+                    <button onClick={avgPlacementMode}>Avg Placement</button>
+                    <button onClick={avgSprMode}>Avg SPR</button>
+                </div>
+                <Line options={options} data={data} />
+            </div>
             }
         </div>
     )
